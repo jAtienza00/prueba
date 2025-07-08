bd905d5181082f834c3c4ba136b067af7b696345
Jorge Atienza <158172073+jAtienza00@users.noreply.github.com>
2025-07-08 20:41:47 +0200

    Update ImagenAniversario.js

--- src/components/ImagenAniversario.js
+++ src/components/ImagenAniversario.js
@@ -53,7 +53,7 @@ const ImagenAniversario = () => {
         </g>
 
         {/* Texto más arriba y más grande, sin negrita */}
-        <text x="45" y="130" fontSize="12" fill="white">para ti</text>
+        <text x="45" y="230" fontSize="12" fill="white">para ti</text>
       </svg>
     </div>
   );
